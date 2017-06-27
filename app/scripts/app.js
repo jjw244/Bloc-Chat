b1a6813dc51e29e7155d9cd893242cec683ce122
Jennifer washburn <jjw244@users.noreply.github.com>
2017-06-27 14:38:03 -0500

    Updated styling - added navbar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,23 +9,14 @@
     $stateProvider
     .state('home', {
        url: '/',
-//       controller: 'HomeCtrl as home',
        templateUrl: '/templates/home.html'
-    })
-    
-    $stateProvider
-    .state('about', {
-       url: '/about',
-//       controller: 'HomeCtrl as home',
-       templateUrl: '/templates/about.html'
-    })
+    });
     
     $stateProvider
     .state('navbar', {
        url: '/navbar',
-//       controller: 'HomeCtrl as home',
        templateUrl: '/templates/navbar.html'
-    })
+    });
        
     $stateProvider
      .state('room', {
@@ -36,6 +27,6 @@
      };
  
     angular
-         .module('chatterBox', ['ui.router', 'firebase', 'ui.bootstrap', 'ngCookies'])
-         .config(config);
+      .module('chatterBox', ['ui.router', 'firebase', 'ui.bootstrap', 'ngCookies'])
+      .config(config);
  })();
